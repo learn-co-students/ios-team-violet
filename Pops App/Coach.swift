5531a4a2096c8cb66906976c40743f618f632f82
Jeffrey Eugene Hoch <jeffreyeugenehoch@gmail.com>
2017-04-04 17:42:57 -0400

    add Coaches and DataStore

--- Pops App/Coach.swift
+++ Pops App/Coach.swift
@@ -1,8 +1,10 @@
 
 import Foundation
+import UIKit
 
 struct Coach {
     let name: String
+    let icon: UIImage?
     let difficulty: DifficultySetting
     let tapStatement: String
     let setSessionStatements: [String]
