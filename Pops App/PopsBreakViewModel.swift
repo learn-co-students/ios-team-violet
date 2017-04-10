7aa261ffb1643006cf38cebf6a258f301fc5333b
Jeffrey Eugene Hoch <jeffreyeugenehoch@gmail.com>
2017-04-10 12:29:15 -0400

    add button functionality to pops break

--- Pops App/PopsBreakViewModel.swift
+++ Pops App/PopsBreakViewModel.swift
@@ -5,8 +5,20 @@ final class PopsBreakViewModel {
     
     lazy var dataStore = DataStore.singleton
     let manager = PopsBreakManager()
-
-    init(){}
+    let defaults = UserDefaults.standard
+    
+    var currentVideoIndex = 0
+    
+    var likedVideoIDs: [String]
+    var dislikedVideoIDs: [String]
+    
+    var userLiked = false
+    var userDisliked = false
+    
+    init(){
+        likedVideoIDs = defaults.value(forKey: "likedVideoIDs") as? [String] ?? []
+        dislikedVideoIDs = defaults.value(forKey: "dislikedVideoIDs") as? [String] ?? []
+    }
     
     
     func letPopsGetYouAVideo(completion: @escaping (String)-> ()) {
@@ -15,5 +27,34 @@ final class PopsBreakViewModel {
         }
     }
     
+    func letPopsGetYouADifferentVideo() -> Int {
+        if userLiked == true {
+            likedVideoIDs.append(manager.popsVideos[currentVideoIndex].id)
+            defaults.set(likedVideoIDs, forKey: "likedVideoIDs")
+        }
+        
+        if userDisliked == true {
+            dislikedVideoIDs.append(manager.popsVideos[currentVideoIndex].id)
+            manager.popsVideos.remove(at: currentVideoIndex)
+            defaults.set(dislikedVideoIDs, forKey: "dislikedVideoIDs")
+        }
+        
+        currentVideoIndex += 1
+        
+        if currentVideoIndex == manager.popsVideos.count {
+            currentVideoIndex = 0
+        }
+        
+        return currentVideoIndex
+    }
     
+    func userLikedVideo() {
+        userDisliked = false
+        userLiked = true
+    }
+    
+    func userDislikedVideo() {
+        userLiked = false
+        userDisliked = true
+    }
 }
